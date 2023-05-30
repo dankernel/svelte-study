44fe66269a7046a97c79e71465ea0fa409f0643d
dankernel <dkdkernel@gmail.com>
2023-05-30 23:56:13 +0900

    FIX: typo fix

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,9 +2,7 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
 const config = {
-	plugins: [//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-    //But others, like autoprefixer, need to run after,
-    tailwindcss(), //Some plugins, like tailwindcss/nesting, need to run before Tailwind, tailwindcss(), //But others, like autoprefixer, need to run after, autoprefixer, autoprefixer, autoprefixer]
+	plugins: [tailwindcss(), autoprefixer]
 };
 
 module.exports = config;
